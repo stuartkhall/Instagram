eebc14a8281572188c50c57da5bf4260555fc157
Stuart Hall <stuartkhall@gmail.com>
2011-11-26 06:45:08 +0800

    Added blank project

---
+++ InstaObjCExample/Classes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InstaObjC
+//
+//  Created by Stuart Hall on 26/11/11.
+//  Copyright (c) 2011 Bytesize. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ InstaObjCExample/Classes/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  InstaObjC
+//
+//  Created by Stuart Hall on 26/11/11.
+//  Copyright (c) 2011 Filter Squad. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}

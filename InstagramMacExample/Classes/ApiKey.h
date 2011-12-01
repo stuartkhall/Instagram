3f69a2d0a4d2e35e344e7261bf55368585930f40
Stuart Hall <stuartkhall@gmail.com>
2011-12-01 21:55:33 +0800

    Renamed example to include Mac in the name

---
+++ InstagramMacExample/Classes/ApiKey.h
@@ -0,0 +1,15 @@
+//
+//  ApiKey.h
+//  Instagram
+//
+//  Created by Stuart Hall on 26/11/11.
+//  Copyright (c) 2011 Bytesize. All rights reserved.
+//
+
+#ifndef Instagram_ApiKey_h
+#define Instagram_ApiKey_h
+
+static NSString* const kInstagramClientId = @"66dc11a658da450898001dde66f3d52d";
+static NSString* const kInstagramCallbackUrl = @"http://www.stuartkhall.com/authenticated";
+
+#endif
